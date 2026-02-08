b20add348f94516951e9dfbbf8f7df95b3ea35c1
Wiktor Wilczewski <wiktorczewski@gmail.com>
2026-02-08 19:28:14 +0100

    feat: add prisma

---
+++ src/lib/db.ts
@@ -0,0 +1,25 @@
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+
+
+const prismaClient = () => {
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({
+        connectionString,
+        max: 20,
+    })
+    const adapter = new PrismaPg(pool);
+    return new PrismaClient({
+        adapter,
+        log: ['query', 'error', 'warn'],
+    });
+};
+
+
+declare global {
+    var prisma: undefined | ReturnType<typeof prismaClient>;
+}
+
+export const db = globalThis.prisma || prismaClient();
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
