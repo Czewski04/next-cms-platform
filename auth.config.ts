427a0901a7333cd6e3d50986e79cb5b9a3b78e22
Wiktor Wilczewski <wiktorczewski@gmail.com>
2026-02-11 23:09:52 +0100

    fix(auth): middleware to proxy migration

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,11 @@
 import type { NextAuthConfig } from "next-auth";
 
-export const authConfig = { pages: { signIn: "/auth/login" },
+export const authConfig = { pages: { signIn: "/admin/login" },
     callbacks: {
         authorized({auth, request: {nextUrl}}) {
             const isLoggedIn = !!auth?.user;
-            const isAdminPanel = nextUrl.pathname.startsWith("/admin");
-            if (isAdminPanel) {
+            const isOnAdminPanel = nextUrl.pathname.startsWith("/admin");
+            if (isOnAdminPanel) {
                 if (nextUrl.pathname == "/admin/login") return true;
                 if (isLoggedIn) return true;
                 return false;
--- src/actions/auth-actions.ts
+++ src/actions/auth-actions.ts
@@ -3,7 +3,7 @@
 import { signIn } from "@/lib/auth";
 import { AuthError } from "next-auth";
 
-export async function authenticate(
+export async function login(
     prevState: string | undefined,
     formData: FormData
 ) {
