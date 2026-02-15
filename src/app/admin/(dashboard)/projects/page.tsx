86fe237f34f6147c0b461ab241b91a1e3535955e
Wiktor Wilczewski <82906404+Czewski04@users.noreply.github.com>
2026-02-15 03:21:54 +0100

    feat(ui): add create project button in admin dashboard

--- src/app/admin/(dashboard)/projects/page.tsx
+++ src/app/admin/(dashboard)/projects/page.tsx
@@ -1,7 +1,24 @@
+import { Plus } from "lucide-react";
+import Link from "next/link";
+
 export default function ProjectsPage(){
     return (
-        <div>
-            All Projects
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <Link href="/admin/projects/create" className="group relative border-2 border-dashed border-zinc-600 flex flex-col justify-center items-center rounded-2xl transition-all">
+                <div>
+                    <Plus size={90} className="transition-all duration-500 group-hover:scale-120 group-hover:text-zinc-600"/>
+                </div>
+                <div className="absolute inset-0 flex items-center justify-center backdrop-blur-sm rounded-2xl opacity-0 transition-opacity duration-500 group-hover:opacity-100">
+                    <span className="font-bold font-inter">Dodaj nowy projekt</span>
+                </div>
+            </Link>
+            
+            <div className="border-2 flex justify-center items-center rounded-2xl"> Project </div>
+            <div className="border-2 flex justify-center items-center rounded-2xl"> Project </div>
+            <div className="border-2 flex justify-center items-center rounded-2xl"> Project </div>
+            <div className="border-2 flex justify-center items-center rounded-2xl"> Project </div>
+            <div className="border-2 flex justify-center items-center rounded-2xl"> Project </div>
+
         </div>
     );
 }
--- src/components/HiddenSidebar.tsx
+++ src/components/HiddenSidebar.tsx
@@ -11,7 +11,7 @@ export default function HiddenSidebar() {
 
     return(
         <div>
-            <button onClick={open} className="lg:hidden"> <Menu/> </button>
+            <button onClick={open} className="transition-transform duration-300 lg:hidden hover:scale-120"> <Menu/> </button>
             <div onClick={close} className={`fixed inset-0 z-40 bg-black/50 backdrop-blur-xs transition-opacity duration-300 ${isOpen ? "opacity-100 visible" : "opacity-0 invisible"}`}/>
                 <div onClick={close} className={`fixed inset-y-0 left-0 z-50 w-55 bg-zinc-800 shadow-2xl rounded-r-3xl transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <div> <Sidebar/></div>
